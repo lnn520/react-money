2b1c140f6b8bad5335c52767fd65d627bea18001
马嘉辉 <335341543@qq.com>
2021-01-25 15:02:55 +0800

    添加图标’ ï

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,21 @@
 import React from 'react';
 import {HashRouter as Router, Link, Redirect, Route, Switch} from 'react-router-dom';
 import styled  from 'styled-components';
+import Nav from './components/Nav';
+
 const Wrapper = styled.div`
 border: 1px solid red;
 height: 100vh;
 display: flex;
 flex-direction: column;
+
 `
 const Main = styled.div`
 border: 1px solid green ;
 flex-grow: 1;
 overflow: auto;
 `
-const Nav = styled.nav`
 
-border: 1px solid blue;
-> ul{
-display: flex;
->li{
-width: 33.3%;
-text-align: center;
-padding: 16px;
-}
-}
-
-`
 function App() {
     return (
         <Router>
@@ -46,19 +37,7 @@ function App() {
                 </Route>
             </Switch>
             </Main>
-            <Nav>
-                <ul>
-                    <li>
-                        <Link to="/tags">标签页</Link>
-                    </li>
-                    <li>
-                        <Link to="/money">记账页</Link>
-                    </li>
-                    <li>
-                        <Link to="/statistics">统计页</Link>
-                    </li>
-                </ul>
-            </Nav>
+            <Nav/>
         </Wrapper>
     </Router>);
 }
