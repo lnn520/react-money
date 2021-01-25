21488b1cdc673bdfe79b70c85d5881c2244e1fb0
马嘉辉 <335341543@qq.com>
2021-01-25 16:04:00 +0800

    盗finish nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,6 +2,11 @@ import styled from 'styled-components';
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+require('icons/money.svg')
+require('icons/lable.svg')
+require('icons/statistics.svg')
+
+
 const NavWrapper = styled.nav`
 line-height: 24px;
 box-shadow: 0 0 3px rgba(0,0,0,0.25);
@@ -10,22 +15,38 @@ display: flex;
 >li{
 width: 33.3%;
 text-align: center;
-padding: 16px;
-}
-}
 
+display: flex;
+flex-direction: column;
+padding: 4px 0;
+align-items: center;
+.icon{
+width: 24px;
+height: 24px;
+}
+}
+}
 `
 const Nav = ()=>{
     return(
         <NavWrapper>
             <ul>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#money"/>
+                    </svg>
                     <Link to="/tags">标签页</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#lable"/>
+                    </svg>
                     <Link to="/money">记账页</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#statistics"/>
+                    </svg>
                     <Link to="/statistics">统计页</Link>
                 </li>
             </ul>
