19326ace77fed67b5cc41de308102ae39b56d4b3
马嘉辉 <335341543@qq.com>
2021-01-26 10:19:36 +0800

    âcreate views

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
 import React from 'react';
-import {HashRouter as Router, Link, Redirect, Route, Switch} from 'react-router-dom';
-import styled  from 'styled-components';
-import Nav from './components/Nav';
-import Layout from './components/Layout';
-
-const Wrapper = styled.div`
-border: 1px solid red;
-height: 100vh;
-display: flex;
-flex-direction: column;
-
-`
-const Main = styled.div`
-border: 1px solid green ;
-flex-grow: 1;
-overflow: auto;
-`
+import {HashRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import Tags from './views/Tags';
+import NoMatch from './views/NotMatch';
 
 function App() {
     return (
@@ -38,36 +26,6 @@ function App() {
 
 
     </Router>);
-}
-function NoMatch(){
-
-    return<h2>404</h2>;
-}
-
-function Statistics() {
-    return(
-        <Layout>
-            <h2>Statistics</h2>
-        </Layout>
-
-
-    )
-}
-
-function Tags() {
-    return(
-        <Layout>
-            <h2>Tags</h2>
-        </Layout>
-    )
-}
-
-function Money() {
-    return(
-       <Layout>
-           <h2>Money</h2>
-       </Layout>
-    )
 }
 
 
