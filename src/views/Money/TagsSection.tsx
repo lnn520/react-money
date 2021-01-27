dffbca1bc329f352ebd896cf03cae1c1d300a46f
马嘉辉 <335341543@qq.com>
2021-01-27 10:51:24 +0800

    finish label's choose and create

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const TagsSection = styled.section`
+const Wrapper = styled.section`
 padding: 12px 16px;
 flex-grow: 1;
 background: #ffffff;
@@ -17,6 +18,9 @@ margin: 0 -12px;
   padding: 4px 18px;
   font-size: 14px;
   margin: 8px 12px;
+  &.selected{
+  background: #f60;
+  }
   }
 }
 >button{
@@ -28,4 +32,36 @@ color: #666;
 margin-top: 8px;
 }
 `;
+const TagsSection: React.FC = (props)=>{
+    const [tags,setTags] = useState<string[]>(['衣','食','住','行']);
+    const [selectedTags,setSelectedTags]=useState<string[]>([])
+    const onAddTag = ()=>{
+        const tagName = window.prompt('输入您要添加的标签');
+        if(tagName!==null){
+            setTags([...tags,tagName])
+        }
+    }
+    const onToggleTag=(tag: string)=>{
+        const index = selectedTags.indexOf(tag);
+        if(index >=0){
+            setSelectedTags(selectedTags.filter(t=>t!==tag))
+            //如果tag已被选中，就赋值所有没有被选中的tag，作为新的selectedTags.
+        }else {
+            setSelectedTags([...selectedTags,tag])
+        }
+    }
+    const getClass =(tag:string)=> selectedTags.indexOf(tag)>=0? 'selected':'';
+    return (
+        <Wrapper>
+            <ol>
+                {tags.map(tag=><li key={tag}
+                onClick={ ()=>{onToggleTag(tag);}}
+                className={getClass(tag)}
+                >{tag}</li>)}
+            </ol>
+            <button onClick={onAddTag}>新增标签</button>
+        </Wrapper>
+    )
+}
+
 export default TagsSection;
