b37aa4a5b98cfbf0a87c60f4bcadaf94345b3d69
马嘉辉 <335341543@qq.com>
2021-01-30 17:18:20 +0800

    封装findTag到useTags里

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import React from 'react';
-import {useTags} from '../../useTags';
+import {useTags} from 'useTags';
+import {createId} from 'lib/createId';
 
 const Wrapper = styled.section`
 padding: 12px 16px;
@@ -43,7 +44,7 @@ const TagsSection: React.FC<Props> = (props)=>{
     const onAddTag = ()=>{
         const tagName = window.prompt('输入您要添加的标签');
         if(tagName!==null){
-            setTags([...tags,{id: Math.random(),name:tagName}])
+            setTags([...tags,{id: createId(),name:tagName}])
         }
     }
     const onToggleTag=(tagId:number)=>{
