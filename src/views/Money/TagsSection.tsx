41de47515a9af13e9b6da1d28106f154d167e8a6
马嘉辉 <335341543@qq.com>
2021-01-30 15:41:25 +0800

    将tag:string 重构为tag:{id:number,namr:strin}â

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -34,35 +34,35 @@ margin-top: 8px;
 }
 `;
 type Props = {
-    value: string[];
-    onChange: (selected:string[])=>void;
+    value: number[];
+    onChange: (selected:number[])=>void;
 }
 const TagsSection: React.FC<Props> = (props)=>{
     const {tags,setTags} = useTags()
-    const selectedTags = props.value;
+    const selectedTagIds = props.value;
     const onAddTag = ()=>{
         const tagName = window.prompt('输入您要添加的标签');
         if(tagName!==null){
-            setTags([...tags,tagName])
+            setTags([...tags,{id: Math.random(),name:tagName}])
         }
     }
-    const onToggleTag=(tag: string)=>{
-        const index = selectedTags.indexOf(tag);
+    const onToggleTag=(tagId:number)=>{
+        const index = selectedTagIds.indexOf(tagId);
         if(index >=0){
-            props.onChange(selectedTags.filter(t=>t!==tag))
+            props.onChange(selectedTagIds.filter(t=>t!==tagId))
             //如果tag已被选中，就赋值所有没有被选中的tag，作为新的selectedTags.
         }else {
-            props.onChange([...selectedTags,tag])
+            props.onChange([...selectedTagIds,tagId])
         }
     }
-    const getClass =(tag:string)=> selectedTags.indexOf(tag)>=0? 'selected':'';
+    const getClass =(tagId:number)=> selectedTagIds.indexOf(tagId)>=0? 'selected':'';
     return (
         <Wrapper>
             <ol>
-                {tags.map(tag=><li key={tag}
-                onClick={ ()=>{onToggleTag(tag);}}
-                className={getClass(tag)}
-                >{tag}</li>)}
+                {tags.map(tag=><li key={tag.id}
+                onClick={ ()=>{onToggleTag(tag.id);}}
+                className={getClass(tag.id)}
+                >{tag.name}</li>)}
             </ol>
             <button onClick={onAddTag}>新增标签</button>
         </Wrapper>
