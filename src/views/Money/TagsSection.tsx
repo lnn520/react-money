6146705b6e97774a86a6e1178df4a460dfc919ac
马嘉辉 <335341543@qq.com>
2021-01-30 10:10:15 +0800

    抽离useTagsââ

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-import React, {useState} from 'react';
+import React from 'react';
+import {useTags} from '../../useTags';
 
 const Wrapper = styled.section`
 padding: 12px 16px;
@@ -37,7 +38,7 @@ type Props = {
     onChange: (selected:string[])=>void;
 }
 const TagsSection: React.FC<Props> = (props)=>{
-    const [tags,setTags] = useState<string[]>(['衣','食','住','行']);
+    const {tags,setTags} = useTags()
     const selectedTags = props.value;
     const onAddTag = ()=>{
         const tagName = window.prompt('输入您要添加的标签');
